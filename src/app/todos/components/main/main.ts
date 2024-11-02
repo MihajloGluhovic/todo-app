c9bc8f8b4b004604bfc961de4872bd66aa10d485
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2024-11-02 13:51:38 +0100

    Implementing todo filters

--- src/app/todos/components/main/main.ts
+++ src/app/todos/components/main/main.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TodosService } from '../../services/todos.service';
 import { CommonModule } from '@angular/common';
+import { FilterEnum } from '../../types/filter.enum';
 
 @Component({
   selector: 'app-todos-main',
@@ -10,4 +11,16 @@ import { CommonModule } from '@angular/common';
 })
 export class MainComponent {
   todosService = inject(TodosService);
+
+  visibleTodos = computed(() => {
+    const todos = this.todosService.todosSig();
+    const filter = this.todosService.filterSig();
+
+    if (filter === FilterEnum.active) {
+      return todos.filter((todo) => !todo.isCompleted);
+    } else if (filter === FilterEnum.completed) {
+      return todos.filter((todo) => todo.isCompleted);
+    }
+    return todos;
+  });
 }
